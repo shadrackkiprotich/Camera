c3dba6df8edcc9039e8a677a354a9749d532a764
Virgil John Lomocso <vjlomocso@gmail.com>
2019-01-19 23:57:34 +0800

    Initial commit

---
+++ Camera/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Camera
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Camera
+A camera API for Xamarin.Forms that allows you to get the data of frame data instead of assuming you want to save it in a file.
